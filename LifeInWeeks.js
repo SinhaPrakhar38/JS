e91ff72b4411e286f682e285fa984f65bcd0ada9
Prakhar Sinha <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-06-03 21:21:42 +0530

    Create LifeInWeeks.js

---
+++ LifeInWeeks.js
@@ -0,0 +1,12 @@
+function lifeInWeeks(age) {
+ 
+    var days = (90-age)*365;
+    var months = (90-age)*12;
+    var years = (90-age);
+    alert("You have "+days+" days, "+months+" months and "+years+" years left.");
+  
+}
+
+lifeInWeeks(80);
+    
+
