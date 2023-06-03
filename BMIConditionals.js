a51150af25f5ced73fc70320fd19e32dcfaa9872
Prakhar Sinha <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-06-03 22:37:25 +0530

    Create BMIConditionals.js

---
+++ BMIConditionals.js
@@ -0,0 +1,19 @@
+function bmiCalculator (weight, height) {
+    var weight; 
+    var height;
+    var bmi = weight/(height*height); 
+    var interpretation;
+    if (bmi<18.5){
+        interpretation = "Your BMI is " + bmi + " so you are underweight";
+    }
+    else if( bmi>=18.5 && bmi<=24.9){
+        interpretation = "Your BMI is " + bmi + " so you have a normal weight";
+    }
+    else{
+        interpretation = "Your BMI is " + bmi + " so you are overweight";
+    }
+    return interpretation;
+}
+
+bmiCalculator(80,2);
+
