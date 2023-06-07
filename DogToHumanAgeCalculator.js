950fac8bcad4e863ab88be3fa6fa48510724a3b5
Prakhar Sinha <74342849+SinhaPrakhar38@users.noreply.github.com>
2023-06-07 21:05:22 +0530

    Update DogToHumanAgeCalculator.js

--- DogToHumanAgeCalculator.js
+++ DogToHumanAgeCalculator.js
@@ -2,7 +2,7 @@
 var dogAge = prompt();
 
 //converting the age to human age with help of pre-defined formula, also using the precedence with parenthesis// 
-var humanAge = ((dogAge-2)*4) +21;
+var humanAge = ((dogAge-2)*4) + 21;
 
 //creating alert to send the output back to user//
 alert("The Human Age of your dog is "+humanAge);
